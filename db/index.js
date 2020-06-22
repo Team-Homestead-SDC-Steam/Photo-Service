6fbe75e2ea4c56a3ded5b03b8e5b47528c03a9e0
William Hudson <williamdhudson@gmail.com>
2020-06-22 14:21:01 -0400

    Saved picture urls to database

--- db/index.js
+++ db/index.js
@@ -10,3 +10,24 @@ db.on('error', function (err) {
 db.once('open', function () {
   console.log('mongoose connected successfully');
 });
+
+var mediaSchema = mongoose.Schema({
+  id: {type: Number, unqiue: true},
+  mediaType: String,
+  url: String
+})
+
+var Item = mongoose.model('Item', mediaSchema);
+
+var insertPictures = () => {
+  for (let i = 1; i < 81; i++) {
+    var newItem = new Item({
+      id: i,
+      mediaType: 'photo',
+      url: `https://loremflickr.com/320/240?lock=${i}`
+    })
+    newItem.save()
+  }
+}
+
+// insertPictures();
