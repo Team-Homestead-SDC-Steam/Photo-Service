89fef24b3b3c356dd7513001f1161c0ea65d0342
Damien Jones <damienjones@gmail.com>
2020-09-09 15:25:25 -0700

    seeding Mongo successfully, if slowly

--- db/seedFile.js
+++ db/seedFile.js
@@ -1,59 +1,70 @@
 const fs = require('fs');
-const {videos: videoArray} = require('./videoData.js')
-const {pictures: photoArray} = require('./imageData.js');
+let {videos} = require('./videoData.js')
+let {pictures} = require('./imageData.js');
 
 const rnd = n => Array.isArray(n) ? n[rnd(n.length) - 1] : Math.floor(Math.random() * n) + 1;
 
+// ** fix source data
+videos = videos.map( video => video.id.videoId);
+pictures = pictures.map( picture => `${picture.public_id}.${picture.format}`)
+
+
 const generateVideos = (count) => {
-  let videoText = '';
+  let str = '';
   let randomVideo;
   for (let i=1; i<=count; i++) {
-    randomVideo = rnd(videoArray);
-    videoText += '{"mediaType":"video",';
-    videoText += `"url":"https://www.youtube.com/embed/${randomVideo.id.videoId}",`;
-    videoText += `"thumbnail":"${randomVideo.snippet.thumbnails.default.url}"}`;
-    if(i!=count) videoText += ','; // add a comma to all but the last one
+    randomVideo = rnd(videos);
+    str += '{"mediaType":"video",';
+    // str += `"url":"https://www.youtube.com/embed/${randomVideo.id.videoId}",`;
+    // str += `"thumbnail":"${randomVideo.snippet.thumbnails.default.url}"}`;
+    str += `"url":"https://www.youtube.com/embed/${randomVideo}",`;
+    str += `"thumbnail":"https://i.ytimg.com/vi/${randomVideo}/default.jpg"}`;
+
+    if(i!=count) str += ','; // add a comma to all but the last one
   }
-  return videoText;
+  return str;
 }
 
 const generatePhotos = (count) => {
-  let photoText = '';
+  let str = '';
   let randomPhoto;
   for (let i=1; i<=count; i++) { 
-    randomPhoto = rnd(photoArray);
-    photoText += '{"mediaType":"carouselPhoto",';
-    photoText += '"url":"https://res.cloudinary.com/dq3iywusm/image/upload/w_600,h_337/'
-    photoText += `${randomPhoto.public_id}.${randomPhoto.format}"}`;
-    if(i!=count) photoText += ',';  // add a comma to all but the last one
+    randomPhoto = rnd(pictures);
+    str += '{"mediaType":"carouselPhoto",';
+    // str += '"url":"https://res.cloudinary.com/dq3iywusm/image/upload/w_600,h_337/'
+    // str += `${randomPhoto.public_id}.${randomPhoto.format}"}`;
+    str += `"url":"https://res.cloudinary.com/dq3iywusm/image/upload/w_600,h_337/${randomPhoto}"}`
+    if(i!=count) str += ',';  // add a comma to all but the last one
   }
-  return photoText;
+  return str;
 }
 
 const generateGame = (id) => {
-  let game = `{"id":${id}, "assets":[`;
-  game += generateVideos(2) + ',';
-  game += generatePhotos(6);
-  return game += ']}' 
+  let str = `{"id":${id}, "assets":[`;
+  str += generateVideos(2) + ',';
+  str += generatePhotos(6);
+  return str += ']}' 
 }
 
-const saveGameData = (count, chunkSize = 10) => {
+const saveGameData = async (count, chunkSize = 2000) => {
+  let path = './assets.dat';
   let chunk = '';
-  // fs.unlinkSync('./assets.dat') // delete file (if exists?)
-  var ts = process.hrtime()
-  let gameStream = fs.createWriteStream('./assets.dat')
+
+  if (fs.existsSync(path)) fs.unlinkSync(path);
+  let gameStream = fs.createWriteStream(path);
+
+  const startTime = Date.now();
   for (let i = 1; i <= count; i++) {
     chunk += `${generateGame(i)}\n`
-    if (i % chunkSize === 0) {
-      gameStream.write( chunk );
-      chunk = '';
+    if (i % chunkSize === 0 || i === count) {
+      if (!gameStream.write(chunk) ) {
+        await new Promise(resolve => gameStream.once('drain', resolve));
+        chunk = '';
+      }
     }
-    // gameStream.write( `${generateGame(i)}\n` );
   }
-
-  gameStream.end(chunk)
-  console.log('Time elapsed: ' + process.hrtime(ts)[1] / 1000000) 
-
+  console.log(`Seeded ${count} records. Operation took ${ (Date.now() - startTime) / 1000} seconds chunked at ${chunkSize}.`) 
+  gameStream.end()
 }
 
-saveGameData(2000000, 100);
+saveGameData(100000);
