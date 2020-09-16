64e6dda7e36c15db3a9dae90a81c417b4bc11932
Damien Jones <damienjones@gmail.com>
2020-09-16 16:43:01 -0700

    fixing Express, API, and React code to allow the UI to display a carousel from live data

--- db/benchmarkMongo.js
+++ db/benchmarkMongo.js
@@ -25,8 +25,32 @@ const testReads = (records = 5000) => {
                     } seconds.\nAverage per record:  ${ Math.round(perRecord * 100000) / 1000
                     }ms. | rpm: ${ Math.round(3600 / perRecord ) } | rps: ${ Math.round(60 / perRecord ) }`);
   });
+}
 
-}  
+//   const testUpdates = (records = 5000) => {
+
+//     // there are only so many promises JS can keep track of, I guess
+//     if (records > 50000) return console.log('Max # of records for this function is 50000.') 
+  
+//     // build the seed queries (already tried with a bunch of different data slices)
+//     const searchIds = Array(records).fill( rnd(10000000) ) 
+    
+//     const startTime = Date.now();
+//     // start all the queries, which return promises
+//     const readPromises =  searchIds.map( id => readGame(id)  ) 
+  
+//     Promise.all(readPromises) 
+//       .then ( (games) => {
+//         const finalTime = (Date.now() - startTime) / 1000;
+//         const perRecord = finalTime / records
+//         console.log(`Validating: last record of search IDs = ${searchIds[records-1]
+//                       }; last record of game data =  ${ games[records-1].id
+//                       }\n${ games.length } game IDs queried & returned in ${ finalTime
+//                       } seconds.\nAverage per record:  ${ Math.round(perRecord * 100000) / 1000
+//                       }ms. | rpm: ${ Math.round(3600 / perRecord ) } | rps: ${ Math.round(60 / perRecord ) }`);
+//     });
+
+// }  
 
 testReads(1000);
 testReads(5000);
--- db/index.js
+++ db/index.js
@@ -8,42 +8,7 @@ const readAllGames = async () => await Game.find();
 const deleteAllGames = async () => await Game.deleteMany();
 
 
-
-
-/* OLD INDEX CONTENT */
-
-//   const numberOfVideos = 200;
-//   const numberOfCarouselPhotos = 600;
-  
-//   var mediaSchema = mongoose.Schema({
-//     id: { type: Number, unique: true },
-//     gameId: Number,
-//     mediaType: String,
-//     url: String
-//   })
-  
-//   var Item = mongoose.model('Item', mediaSchema);
-
-//   // Picks 2 videos and 6 carousel photos from the database
-//   var generateIds = (id) => {
-//     var idArr = []
-//     idArr.push((id) * 2, (id) * 2 + 1)
-//     idArr.push(200 + id * 6, 200 + id * 6 + 1, 200 + id * 6 + 2, 200 + id * 6 + 3, 200 + id * 6 + 4, 200 + id * 6 + 5)
-//     return idArr
-//   }
-
-var getMedia = (param, callback) => {
-  var Ids = generateIds(param)
-  Item.find().where('id').in(Ids).exec((err, data) => {
-    if (err) {
-      console.log('error with getMedia in db file: ', err)
-    }
-    callback(null, data)
-  })
-}
-
 module.exports.db = db;
-module.exports.getMedia = getMedia;
 module.exports.Game = Game;
 module.exports.createGame = createGame;
 module.exports.readGame = readGame;
